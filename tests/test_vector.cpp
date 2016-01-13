0680c9bfb8005d0073dd9886441b5765a8d2b64c
Nikolay2015 <mushroom8613@gmail.com>
2016-01-14 01:33:55 +0300

    start 3

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "array.h"
+#include "vector.h"
 #include "catch.hpp"
 
 template <class T>
